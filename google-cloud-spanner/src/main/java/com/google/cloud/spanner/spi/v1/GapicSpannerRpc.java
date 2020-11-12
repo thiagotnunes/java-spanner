7013ce01506be84cab95c0ae0b513e93cecf0d8d
Olav Loite <koloite@gmail.com>
2020-11-12 08:13:31 +0100

    fix: set null instead of default instance

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/spi/v1/GapicSpannerRpc.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/spi/v1/GapicSpannerRpc.java
@@ -93,7 +93,6 @@ import com.google.spanner.admin.database.v1.Database;
 import com.google.spanner.admin.database.v1.DatabaseAdminGrpc;
 import com.google.spanner.admin.database.v1.DeleteBackupRequest;
 import com.google.spanner.admin.database.v1.DropDatabaseRequest;
-import com.google.spanner.admin.database.v1.EncryptionConfig;
 import com.google.spanner.admin.database.v1.GetBackupRequest;
 import com.google.spanner.admin.database.v1.GetDatabaseDdlRequest;
 import com.google.spanner.admin.database.v1.GetDatabaseRequest;
@@ -848,7 +847,7 @@ public class GapicSpannerRpc implements SpannerRpc {
             .addAllExtraStatements(additionalStatements)
             .setEncryptionConfig(
                 databaseInfo.getEncryptionConfigInfo() == null
-                    ? EncryptionConfig.getDefaultInstance()
+                    ? null
                     : databaseInfo.getEncryptionConfigInfo().toProto())
             .build();
 
